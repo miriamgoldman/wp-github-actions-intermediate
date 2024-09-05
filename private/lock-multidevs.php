5613bac0d3e71176fa630eefa6681188b2759aa1
Miriam Goldman <miriamgoldman@pantheon.io>
2024-09-05 08:37:23 -0400

    qs script

---
+++ private/lock-multidevs.php
@@ -0,0 +1,24 @@
+<?php
+// Auto-lock new environments.
+
+if ( defined( 'PANTHEON_ENVIRONMENT' ) && isset( $_POST['environment'] ) ) {
+
+    $data = json_encode(
+      [
+        'type' => 'lock_environment',  // Workflow type.
+        'environment' => PANTHEON_ENVIRONMENT,
+        'params' => [
+          "username" => PANTHEON_SITE_NAME,
+          "password" => PANTHEON_SITE_NAME . '-' . PANTHEON_ENVIRONMENT,
+        ]
+      ]
+    );
+
+    var_dump( $data );
+    
+    /*
+    echo "--- Start workflow: lock_environment -- \n\n";
+    $result = pantheon_curl('https://api.live.getpantheon.com/sites/self/environments/self/workflows', $data, 8443, 'POST');
+    echo "--- End workflow: lock_environment -- \n\n";
+    */
+}
